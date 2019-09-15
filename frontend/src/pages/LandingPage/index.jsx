bcd94d1d9135081aeb10792858939d0fccfe6aa1
Acho Arnold <a.ewin@geophy.com>
2019-09-15 21:32:08 +0200

    Created base configuration

---
+++ frontend/src/pages/LandingPage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.scss';
+
+const LandingPage = props => {
+  return (
+    <div className="app w-full h-full flex">
+      <div className="md:w-1/3 flex items-center">
+        <h2 className="w-full text-center">
+          How to create a react app
+        </h2>
+      </div>
+      <div className="md:w-2/3 bg-green-700">
+
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
