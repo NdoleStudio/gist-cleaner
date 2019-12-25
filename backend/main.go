fd991917197f64b8b0b4a99c94c23606a8630daf
Acho Arnold <a.ewin@geophy.com>
2019-12-25 23:27:32 +0100

    Added backend file in golang

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,207 @@
-package backend
+package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
+
+const API_VERSION = "v1"
+const JSON_CONTENT_TYPE = "application/json"
+const DASHBOARD_DATA_QUERY = "{\"query\": \"query {viewer{login bio avatarUrl url name gists(first:50,orderBy:{field:CREATED_AT,direction:DESC}){edges{node{id files{name} description url updatedAt name}}}}}\"}"
+
+type accessTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ErrorDescription string `json:"error_description"`
+	Error string `json:"error"`
+}
+
+type DashboardRequest struct {
+	Code string `json:"code"`
+}
+
+type DashboardData struct {
+	Data struct {
+		Viewer struct {
+			Login     string `json:"login"`
+			Bio       string `json:"bio"`
+			AvatarURL string `json:"avatarUrl"`
+			URL       string `json:"url"`
+			Name      string `json:"name"`
+			Gists     struct {
+				Edges []struct {
+					Node struct {
+						ID    string `json:"id"`
+						Files []struct {
+							Name string `json:"name"`
+						} `json:"files"`
+						Description string    `json:"description"`
+						URL         string    `json:"url"`
+						UpdatedAt   time.Time `json:"updatedAt"`
+						Name        string    `json:"name"`
+					} `json:"node"`
+				} `json:"edges"`
+			} `json:"gists"`
+		} `json:"viewer"`
+	} `json:"data"`
+}
+
+type hashMap map[string]string
+
+
+func handleRoot(responseWriter http.ResponseWriter, request *http.Request) {
+	responseObject := map[string]interface{}{
+		"apiVersion": API_VERSION,
+	}
+
+	writeResponseObject(responseWriter, responseObject)
+}
+
+//func notFound(responseWriter http.ResponseWriter, r *http.Request) {
+//	writeResponseObject(responseWriter, map[string]interface{}{"how": "fine"})
+//}
+
+func handleDashboard(responseWriter http.ResponseWriter, request *http.Request) {
+	decoder := json.NewDecoder(request.Body)
+	var dashboardRequest DashboardRequest
+	err := decoder.Decode(&dashboardRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	requestParams := map[string]interface{} {
+		"client_id" : os.Getenv("GITHUB_CLIENT_ID"),
+		"client_secret" : os.Getenv("GITHUB_CLIENT_SECRET"),
+		"code": dashboardRequest.Code,
+	}
+
+	log.Println("Code =" + dashboardRequest.Code)
+
+	apiRequest := createPostRequest(os.Getenv("GITHUB_ACCESS_TOKEN_ENDPOINT"), requestParams)
+
+	client := &http.Client{}
+	apiResponse, _ := client.Do(apiRequest)
+
+	decoder = json.NewDecoder(apiResponse.Body)
+	var githubApiResponse accessTokenResponse
+	err = decoder.Decode(&githubApiResponse)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Access Token =" + githubApiResponse.AccessToken)
+
+	apiRequest = createGraphQlRequest(os.Getenv("GITHUB_GRAPHQL_API"), DASHBOARD_DATA_QUERY, githubApiResponse.AccessToken)
+	apiResponse, _ = client.Do(apiRequest)
+
+	decoder = json.NewDecoder(apiResponse.Body)
+	var apiResponseData DashboardData
+	err = decoder.Decode(&apiResponseData)
+	if err != nil {
+		panic(err)
+	}
+
+	dashboardDataObject := map[string]interface{}{
+		"access_token": githubApiResponse.AccessToken,
+		"error": false,
+		"is_successful": true,
+		"username": apiResponseData.Data.Viewer.Login,
+		"avatar_url": apiResponseData.Data.Viewer.AvatarURL,
+		"name": apiResponseData.Data.Viewer.Name,
+		"bio" : apiResponseData.Data.Viewer.Bio,
+		"url" : apiResponseData.Data.Viewer.URL,
+		"gists": getGistsFromEdges(apiResponseData),
+	}
+
+	writeResponseObject(responseWriter, dashboardDataObject)
+}
+
+func getGistsFromEdges(apiResponseData DashboardData) []hashMap {
+	gists := make([]hashMap, 0)
+	gistData :=apiResponseData.Data.Viewer.Gists.Edges
+
+	for k := range gistData {
+		nodeData := gistData[k].Node;
+		gists = append(gists, map[string]string {
+			"id": nodeData.ID,
+			"file_name": nodeData.Files[0].Name,
+			"description": nodeData.Description,
+			"timestamp": strconv.FormatInt(nodeData.UpdatedAt.Unix(), 10),
+			"name": nodeData.Name,
+			"url": nodeData.URL,
+		})
+	}
+
+	return gists
+}
+
+func createPostRequest(url string, requestParams map[string]interface{}) *http.Request {
+	apiRequest, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonEncodeMap(requestParams)))
+	if err != nil {
+		log.Println("Cannot create request")
+	}
+	apiRequest.Header.Set("Accept", JSON_CONTENT_TYPE)
+	apiRequest.Header.Set("Content-Type", JSON_CONTENT_TYPE)
+
+	return apiRequest
+}
+
+func createGraphQlRequest(url string, query string, token string) *http.Request {
+	apiRequest, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(query)))
+	if err != nil {
+		log.Println("Cannot create request")
+	}
+
+	apiRequest.Header.Set("Authorization", "bearer " + token)
+	apiRequest.Header.Set("Accept", JSON_CONTENT_TYPE)
+	apiRequest.Header.Set("Content-Type", JSON_CONTENT_TYPE)
+
+	return apiRequest
+}
+
+func jsonEncodeMap(mapObject map[string]interface{}) []byte {
+	encodedObject, _ := json.Marshal(mapObject)
+	return encodedObject
+}
+
+func writeResponseObject(responseWriter http.ResponseWriter, responseObject map[string] interface{}) {
+	responseWriter.Header().Set("Content-Type", JSON_CONTENT_TYPE)
+	responseWriter.Header().Set("Access-Control-Allow-Origin", "*")
+	responseWriter.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	responseWriter.WriteHeader(http.StatusOK)
+
+	responseObjectInBytes, err := json.Marshal(responseObject)
+	if err != nil {
+		log.Fatalln("Cannot convert object to string")
+	}
+
+	_, err = responseWriter.Write(responseObjectInBytes)
+	if err != nil {
+		log.Fatalln("Cannot write response")
+	}
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleRoot)
+
+	api := r.PathPrefix("/" + API_VERSION).Subrouter()
+	api.HandleFunc("/", handleRoot)
+	api.HandleFunc("/dashboard", handleDashboard).Methods(http.MethodPost)
+	api.HandleFunc("/dashboard", handleRoot).Methods(http.MethodOptions)
+
+	log.Println("Server started")
+	log.Fatalln(http.ListenAndServe(":8080", r))
 }
--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -5,13 +5,16 @@ import '@mdi/font/css/materialdesignicons.min.css';
 import logo from '../../images/logo.png'
 import iconDelete from '../../images/icon-delete.png';
 import closeIcon from '../../images/close-icon.svg';
-import {ROUTE_LANDING_PAGE, BASE_INPUT_CLASS, BASE_BUTTON_CLASS} from "../../constants/constants";
+import {ROUTE_LANDING_PAGE, BASE_INPUT_CLASS, BASE_BUTTON_CLASS, API_ENDPOINT_DASHBOARD} from "../../constants/constants";
 import CheckBox from '../../components/CheckBox';
 import postscribe from 'postscribe';
 import {API_RESPONSE} from "../../services/api";
 import moment from "moment";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+import axios from "axios";
+
+
 
 class Dashboard extends Component {
   constructor(props) {
@@ -20,12 +23,13 @@ class Dashboard extends Component {
     this.state = {
       checkedGists: [],
       gists: [],
-      name: 'Acho Arnold',
-      image_url: 'https://avatars3.githubusercontent.com/u/4196457?v=4',
-      username: 'AchoArnold',
+      name: '',
+      avatar_url: '',
+      username: '',
       displayModal: false,
       usernameInput: '',
-      token: null
+      bio: '',
+      access_token: null
     };
 
     this.openDisplayModal = this.openDisplayModal.bind(this);
@@ -39,13 +43,47 @@ class Dashboard extends Component {
   }
 
   componentDidMount() {
+    console.log(window.location.search);
+    console.log(new URLSearchParams(window.location.search).get('code'));
+
     this.setState({
-      gists: API_RESPONSE,
+      gists: API_RESPONSE.gists,
+      name: API_RESPONSE.name,
+      url: API_RESPONSE.url,
+      bio: API_RESPONSE.bio,
+      username: API_RESPONSE.username,
+      avatar_url: API_RESPONSE.avatar_url,
+      access_token: API_RESPONSE.access_token,
     }, () => {
       this.state.gists.map((gist) => {
-        // return postscribe('#' + gist.id, '<script src="https://gist.github.com/AchoArnold/' + gist.id + '.js"></script>');
+        return postscribe('#' + gist.id, '<script src="https://gist.github.com/' + this.state.username + '/' + gist.name + '.js"></script>');
       });
     });
+    // fetch(API_ENDPOINT_DASHBOARD, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Accept': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     code: new URLSearchParams(window.location.search).get('code')
+    //   })
+    // }).then(response => response.json()).then(data => {
+    //   console.log(data);
+    //   this.setState({
+    //     gists: data.gists,
+    //     name: data.name,
+    //     url: data.url,
+    //     bio: data.bio,
+    //     username: data.username,
+    //     avatar_url: data.avatar_url,
+    //     access_token: data.access_token,
+    //   }, () => {
+    //     this.state.gists.map((gist) => {
+    //       // return postscribe('#' + gist.id, '<script src="https://gist.github.com/AchoArnold/' + gist.id + '.js"></script>');
+    //     });
+    //   });
+    // });
   }
 
   handleUsernameInputChange(event) {
@@ -128,32 +166,36 @@ class Dashboard extends Component {
   }
 
   renderDate(dateAsString) {
-    return moment.duration(moment().diff(moment(dateAsString))).humanize() + ' ago';
+    return moment.duration(moment().diff(moment.unix(dateAsString))).humanize() + ' ago';
   }
 
   render() {
     return (
-      <div>
+      <div className="bg-gray-100">
         <header className="flex justify-center bg-black text-white p-2 header">
           <a href={ROUTE_LANDING_PAGE} className="flex align-middle">
             <img src={logo} alt="Gist Cleaner logo" className="page-logo"/>
             <h2 className="text-3xl font-bold ml-3">GitHub Gist Cleaner</h2>
           </a>
         </header>
-        <section className="body-content bg-gray-600 flex md:max-w-5xl mx-auto">
-          <div className="md:w-1/4 bg-red-400 pt-6 px-3">
-            <img src={this.state.image_url} alt="Profile"/>
+        <section className="body-content flex md:max-w-5xl mx-auto">
+          <div className="md:w-1/4 pt-6 px-3">
+            <a href={this.state.url} target="_blank" rel="noopener noreferrer">
+              <img src={this.state.avatar_url} alt="Avatar"/>
+            </a>
             <div className="w-full">
               <h2 className="text-left text-2xl font-bold mt-2">
                 {this.state.name}
-                <br/>
-                <span className="text-gray-700 text-xl font-normal">
-                {this.state.username}
-              </span>
               </h2>
+              <h4 className="text-gray-700 text-xl font-normal -mt-2">
+                {this.state.username}
+              </h4>
+              <p className="text-gray-900 text-sm mt-1">
+                {this.state.bio}
+              </p>
             </div>
           </div>
-          <div className="md:w-3/4 bg-green-100 pt-6 px-3">
+          <div className="md:w-3/4 pt-6 px-3">
             <div className="w-full flex items-center">
               <div className="w-4/12 pl-4">
                 <CheckBox isChecked={this.selectAllIsChecked()} onChange={ () => this.toggleSelectAllCheckbox() } />
@@ -166,32 +208,35 @@ class Dashboard extends Component {
                 </button>
               </div>
             </div>
-            { this.state.gists.map((gist) => {
-              return (
-                <div className="w-full flex mb-3" key={gist.id}>
-                  <div className="w-1/12 pl-4">
-                    <CheckBox isChecked={this.state.checkedGists.includes(gist.id)} onChange={() => this.toggleCheckBox(gist.id)} />
-                  </div>
-                  <div className="w-11/12 header">
-                    <div className="w-full flex bg-gray-300 rounded text-black px-3 border-2 rounded-b-none border-gray-400">
-                      <div className="w-11/12">
-                        <h2 className="font-bold">
-                          {this.getFileName(gist)}
-                        </h2>
-                        <h5 className="text-xs">created {this.renderDate(gist.created_at)}</h5>
-                        <h4 className="text-sm">{gist.description}</h4>
+            {this.state.gists.length === 0 ?(
+              <div className="w-full mt-3">
+                <h2 className="w-full text-center font-bold text-xl">You have no github gists!</h2>
+              </div>
+            ) : (
+              this.state.gists.map((gist) => {
+                  return (
+                    <div className="w-full flex mb-3" key={gist.id}>
+                      <div className="w-1/12 pl-4">
+                        <CheckBox isChecked={this.state.checkedGists.includes(gist.id)} onChange={() => this.toggleCheckBox(gist.id)} />
                       </div>
-                      <div className="w-1/12">
+                      <div className="w-11/12 header">
+                        <div className="w-full flex bg-gray-300 rounded text-black px-3 border-2 rounded-b-none border-gray-400">
+                          <div className="w-11/12">
+                            <h2 className="font-bold">{gist.file_name}</h2>
+                            <h5 className="text-xs">created {this.renderDate(gist.timestamp)}</h5>
+                            <h4 className="text-sm">{gist.description}</h4>
+                          </div>
+                          <div className="w-1/12">
+                          </div>
+                        </div>
+                        <div className="w-full border-b border-gray-300">
+                          <div id={gist.id}></div>
+                        </div>
                       </div>
                     </div>
-                    <div className="w-full border-b border-gray-300">
-                      <div id={gist.id}></div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-
+                  );
+                })
+            )}
           </div>
         </section>
         <div id="resultsModal" style={{display: this.state.displayModal ? 'block' : 'none'}} className="modal">
