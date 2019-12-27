fc4574b503f830e0b10692ba3b919e1958e779f4
Acho Arnold <a.ewin@geophy.com>
2019-12-27 11:06:36 +0100

    Using now.sh domain for API requests

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -9,9 +9,9 @@ export const BASE_INPUT_CLASS = 'text-center w-2/3 mt-3 mb-3 appearance-none bor
 export const BASE_BUTTON_CLASS = 'auth-btn bg-transparent hover:bg-red-500 text-red-700 hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded inline-flex items-center';
 
 
-export const API_ENDPOINT_DASHBOARD = 'http://localhost:8080/v1/dashboard';
+export const API_ENDPOINT_DASHBOARD = 'https://api.gist-cleaner.ndolestudio.com/dashboard';
 
-export const API_ENDPOINT_DELETE = 'http://localhost:8080/v1/delete';
+export const API_ENDPOINT_DELETE = 'https://api.gist-cleaner.ndolestudio.com/delete';
 
 export const PUSHER_APP_KEY = '35a8c7a2a9c1b3685ea7';
 
